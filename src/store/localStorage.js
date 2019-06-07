c4397f02872f26b1414f377444479dda675f9a59
Ryan Warner <warner.ryan@gmail.com>
2019-06-06 20:31:30 -0400

    Add local storage convenience funcs

---
+++ src/store/localStorage.js
@@ -0,0 +1,19 @@
+export const loadState = () => {
+  try {
+    const serializedState = window.localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (error) {
+    console.log(error)
+    return undefined
+  }
+}
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state)
+    window.localStorage.setItem('state', serializedState)
+  } catch (error) {
+    console.log(error)
+  }
+}
