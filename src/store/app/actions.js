c281d9b3e1695ca110aaa4e398830c0d9378f6ac
Ryan Warner <warner.ryan@gmail.com>
2019-06-13 09:47:36 -0400

    Cleanup

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -1,12 +1,8 @@
 export const ActionTypes = {
-  SET_ACTIVE_MODAL: 'SET_ACTIVE_MODAL',
-  SET_FLOATING_PERSON_NUM: 'SET_FLOATING_PERSON_NUM',
-  SET_TOPICS_CAROUSEL_INDEX: 'SET_TOPICS_CAROUSEL_INDEX'
+  SET_ACTIVE_MODAL: 'SET_ACTIVE_MODAL'
 }
 
 export const closeAllModals = () => ({ type: ActionTypes.SET_ACTIVE_MODAL, modal: null })
-export const setFloatingPersonNum = (floatingPersonNum) => ({ type: ActionTypes.SET_FLOATING_PERSON_NUM, floatingPersonNum })
-export const setTopicsCarouselIndex = (topicsCarouselIndex) => ({ type: ActionTypes.SET_TOPICS_CAROUSEL_INDEX, index: topicsCarouselIndex })
 export const openModal = ({ modalName, modalState }) => ({
   type: ActionTypes.SET_ACTIVE_MODAL,
   modal: { name: modalName, modalState }
--- src/store/app/reducer.js
+++ src/store/app/reducer.js
@@ -8,10 +8,6 @@ export default (state = initialState, action) => {
   switch (type) {
     case 'SET_ACTIVE_MODAL':
       return Object.assign({}, state, { activeModal: action.modal })
-    case 'SET_FLOATING_PERSON_NUM':
-      return Object.assign({}, state, { floatingPersonNum: action.floatingPersonNum })
-    case 'SET_TOPICS_CAROUSEL_INDEX':
-      return Object.assign({}, state, { topicsCarouselIndex: action.index })
   }
 
   return state
