bd0a73e11e4d79b9fe057d180f13b497e09970ba
Ryan Warner <warner.ryan@gmail.com>
2019-07-10 20:52:47 -0400

    Remove navThemes for now

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,30 +2,22 @@ import Root from '../components/Root/Root'
 import Home from '../pages/Home'
 import NotFound from '../components/NotFound/NotFound'
 
-import navThemes from './navThemes'
-
-// Navigation is themeable for both mobile and desktop
-// see navThemes.js to customize themes,
-// and set them on each route with navTheme: navThemes.myTheme
-
 export default [{
   component: Root,
   routes: [{
     path: '/',
     exact: true,
     component: Home,
-    navTheme: navThemes.home,
     meta: {
       title: 'Home',
-      description: 'React Sprout v2'
+      description: 'React Starter by Yawnch'
     }
   }, {
     path: '*',
     component: NotFound,
-    navTheme: navThemes.notFound,
     meta: {
       title: 'Not found',
-      description: '404'
+      description: '404 - Not Found'
     }
   }]
 }]
