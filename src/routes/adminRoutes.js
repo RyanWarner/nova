b0228f7777609ddffa0bf6968ed7bf24100403eb
Ryan Warner <warner.ryan@gmail.com>
2019-10-14 08:50:46 -0400

    Remove admin colors and utilities

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,7 +1,5 @@
 import AdminRoot from 'app/components/admin/AdminRoot/AdminRoot'
 import AdminUtilities from 'app/pages/admin/AdminUtilities'
-import AdminIcons from 'app/pages/admin/AdminIcons'
-import AdminColors from 'app/pages/admin/AdminColors'
 
 export default [{
   path: '/admin',
@@ -20,20 +18,6 @@ export default [{
     path: '/admin/utilities',
     exact: true,
     component: AdminUtilities,
-    meta: {
-      title: 'Admin - Icons'
-    }
-  }, {
-    path: '/admin/icons',
-    exact: true,
-    component: AdminIcons,
-    meta: {
-      title: 'Admin - Icons'
-    }
-  }, {
-    path: '/admin/colors',
-    exact: true,
-    component: AdminColors,
     meta: {
       title: 'Admin - Icons'
     }
