5558cd32b497cbff4f2bf9b4d054e3f9dfd5beda
Ryan Warner <warner.ryan@gmail.com>
2019-10-14 08:57:08 -0400

    Fix loading selector

--- src/store/loading/selectors.js
+++ src/store/loading/selectors.js
@@ -1,5 +1,5 @@
 import { some, get } from 'lodash'
 export const createLoadingSelector = (actions) => (state) => {
   // returns true only when all actions are not loading
-  return some((action) => get(state, `loading.${action}`))
+  return some(actions, (action) => get(state, `loading.${action}`))
 }
