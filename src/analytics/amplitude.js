077209a8ee58a1ed070ce9da97bfef56368e475e
Ryan Warner <warner.ryan@gmail.com>
2019-10-15 08:05:43 -0400

    Cleanup

--- src/analytics/amplitude.js
+++ src/analytics/amplitude.js
@@ -1,4 +1,4 @@
-import { analyticsEventTypes } from './analytics'
+import { analyticsEventTypes as types } from './analytics'
 
 const amplitudeApiKey = process.env.AMPLITUDE_API_KEY
 
@@ -7,7 +7,8 @@ export default class AmplitudeAnalytics {
     if (typeof window !== 'undefined') {
       const amplitude = require('amplitude-js')
       this.Amplitude = amplitude.getInstance()
-      this.Amplitude.init(amplitudeApiKey, null, { includeReferrer: true, includeUtm: true })
+      const options = { includeReferrer: true, includeUtm: true }
+      this.Amplitude.init(amplitudeApiKey, null, options)
     }
   }
 
@@ -19,7 +20,7 @@ export default class AmplitudeAnalytics {
       ...props
     }
 
-    this.Amplitude.logEventWithTimestamp(analyticsEventTypes.pageView, eventProps, timestamp)
+    this.Amplitude.logEventWithTimestamp(types.pageView, eventProps, timestamp)
   }
 
   static event = ({ eventType, pathname, props }) => {
