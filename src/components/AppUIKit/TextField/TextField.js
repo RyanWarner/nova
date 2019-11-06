d626f58d4a9fe9f20a1b6e236edfe1ad4e17ce9b
Ryan Warner <warner.ryan@gmail.com>
2019-11-06 16:42:00 -0500

    Convert TextField stories to mdx, add better documentation

--- src/components/AppUIKit/TextField/TextField.js
+++ src/components/AppUIKit/TextField/TextField.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { asField } from 'informed'
 
 import * as S from './styles'
 
-export default asField((props) => {
-  const { label, noError, ...rest } = props
+const TextField = asField(props => {
+  const { label, disableError, ...rest } = props
   const error = props.fieldState.error
 
   return (
     <S.TextFieldComponent>
       {label && <S.Label htmlFor={props.field}>{label}</S.Label>}
       <S.TextInput {...rest} />
-      {!noError && <S.Error>{error}</S.Error>}
+      {!disableError && <S.Error>{error}</S.Error>}
     </S.TextFieldComponent>
   )
 })
+
+const TextFieldWrap = props => <TextField {...props} />
+
+TextFieldWrap.propTypes = {
+  /** The name of the text input */
+  label: PropTypes.string,
+
+  /** When true, the error message is never shown in the UI */
+  disableError: PropTypes.bool
+}
+
+TextFieldWrap.defaultProps = {
+  label: undefined,
+  disableError: false
+}
+
+export default TextFieldWrap
